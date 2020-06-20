b706be58ed4f48513213bfb24e4d13e14c167b4f
Jeremy Fuksa <hello@orangefla.me>
2020-06-19 23:50:50 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,10 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
 ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+rm ~/.zshrc
+cp ./.zshrc ~/.zshrc
+source ~/.zshrc
+cp ./.zprofile ~/.zprofile
+source ~/.zprofile
+update-all
+sudo reboot now

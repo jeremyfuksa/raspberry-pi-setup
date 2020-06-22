52df25a4748aa7426338ae8953b12c97cf0d55d6
Jeremy Fuksa <hello@orangefla.me>
2020-06-21 21:39:30 -0500

    Update plexserver.sh

--- plexserver.sh
+++ plexserver.sh
@@ -8,5 +8,11 @@ echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sour
 sudo apt update
 install plexmediaserver
 sudo cat hostname -I >> /boot/cmdline.txt
-
+wget https://download.expressvpn.xyz/clients/linux/expressvpn_2.5.0.505-1_armhf.deb
+sudo dpkg -i expressvpn_2.5.0.505-1_armhf.deb
+rm expressvpn_2.5.0.505-1_armhf.deb
+echo "You need to get your activation code from expressvpn.com"
+expressvpn activate
+expressvpn autoconnect true
+install transmission transmission-daemon
 sudo reboot now

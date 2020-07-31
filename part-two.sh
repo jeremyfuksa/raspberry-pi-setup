0426312c1d1447e9b03043f1c3ea7f929b5fba2f
Jeremy Fuksa <hello@orangefla.me>
2020-07-31 15:28:20 -0500

    Create part-two.sh
    All the rest of the setup stuff.

---
+++ part-two.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+git clone https://github.com/denysdovhan/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt"
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+rm ~/.zshrc
+cp ./.zshrc ~/.zshrc
+source ~/.zshrc
+cp ./.zprofile ~/.zprofile
+source ~/.zprofile
+install htop
+update-all
+sudo reboot now
